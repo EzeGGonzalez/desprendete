7927612f6e5895ba033c85ffae1f09c1c8bafc20
Ezequiel Gerónimo Gonzalez <ezequiel@adaitw.com.ar>
2017-11-22 00:30:35 -0300

    change navbar color

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -4,7 +4,7 @@
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
     <b-navbar-brand href="/">
-      <img src="/logotipo_desprendete_color_negativo_rojo.svg" alt="Desprendete" height="30">
+      <img src="/logotipo_desprendete_color_negativo_azul.svg" alt="Desprendete" height="30">
     </b-navbar-brand>
 
     <b-collapse is-nav id="nav_collapse">
