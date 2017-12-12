c712c1a60f5e61b905b9bdeb1e9565cc8e453608
Ezequiel Gerónimo Gonzalez <ezequiel@adaitw.com.ar>
2017-12-12 13:13:04 -0300

    use lodash to check if variable is null or undefined

--- server/routes/api/category.js
+++ server/routes/api/category.js
@@ -1,5 +1,5 @@
 var keystone = require('keystone');
-
+var _ = require('lodash');
 var Category = keystone.list('Category');
 
 exports.list = function(req, res) {
@@ -9,7 +9,7 @@ exports.list = function(req, res) {
     .exec(async function(err, items) {
       if (err) return res.json({ err: err });
 
-      let tree = items.filter(c => c.parent === null).map(p => {
+      let tree = items.filter(c => _.isNil(c.parent)).map(p => {
         return {
           ...p,
           subcategories: items.filter(c => p._id.equals(c.parent))
