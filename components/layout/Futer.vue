d05050a18ddc20765dd622d5c4dbd25e552f7120
Ezequiel Gerónimo Gonzalez <ezequiel@adaitw.com.ar>
2018-01-03 16:01:39 -0300

    improve footer layout

--- components/layout/Futer.vue
+++ components/layout/Futer.vue
@@ -1,18 +1,17 @@
 <template lang="pug">
   footer.footer
-    .container-fluid
-      b-row
-        b-col(md='6')
-          p.m-0
-            | Un producto de 
-            a(target='_blank', href='http://www.emprendia.net/')
-              img(src='/logotipo_emprendia_horizontal_blanco.png')
-        b-col.text-right(md='6')
-          p.m-0 Clasificados gratis para regalar lo que ya no usas. &copy; {{new Date().getFullYear()}}
+    .container-fluid.d-flex.justify-content-between.flex-column.flex-md-row
+      .product-by
+        p.m-0
+          | Un producto de 
+          a(target='_blank', href='http://www.emprendia.net/')
+            img(src='/logotipo_emprendia_horizontal_blanco.png')
+      .product-copyright
+        p.m-0 Clasificados gratis para regalar lo que ya no usas. &copy; {{new Date().getFullYear()}}
 </template>
 
 <style lang="sass" scoped>
-@import "~assets/scss/color"
+@import "~assets/scss/resources.scss"
 
 footer
   color: white
@@ -21,6 +20,24 @@ footer
   line-height: 60px
   background-color: $brand-primary
 
+  .product-by
+    text-align: left
+  .product-copyright
+    text-align: right
+
+  @include media-breakpoint-down(sm)
+    height: auto
+    line-height: 1.5
+    padding-top: 1.5rem
+    padding-bottom: 1.5rem
+
+    .product-by
+      text-align: center
+      padding-bottom: 0.75rem
+    .product-copyright
+      text-align: center
+
   img
     height: 30px
+
 </style>
